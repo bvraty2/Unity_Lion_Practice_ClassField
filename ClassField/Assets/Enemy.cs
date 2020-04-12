655c6e4136694ed6f790d22108fbfd13eb2363b0
bvraty2 <bvraty2@gmail.com>
2020-04-12 19:14:07 +0800

    練習作業

--- ClassField/Assets/Enemy.cs
+++ ClassField/Assets/Enemy.cs
@@ -1,17 +1,61 @@
 ﻿using UnityEngine;
 
-public class Enemy
-   : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Enemy : MonoBehaviour
+{ /// <summary>
+   /// 怪物的血量
+   /// </summary>
+   [Header("血量"), Range(50, 100)]
+    [Tooltip("怪物的血量")]
+    public int hp  ;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /// <summary>
+   /// 怪物的魔力
+   /// </summary>
+   [Header("魔力"), Range(10, 100)]
+    [Tooltip("怪物的魔力")]
+    public int mp = 100;
+
+    /// <summary>
+    /// 怪物的移動速度
+    /// </summary>
+	[Header("怪物的移動速度"), Range(1, 50.5f)]
+    public float ms = 50.5f;
+
+    /// <summary>
+    /// 怪物的攻擊力
+    /// </summary>
+	[Range(20, 200)]
+    public int ma = 50;
+
+    /// <summary>
+    /// 怪物的防禦力
+    /// </summary>
+	[Range(0, 100)]
+    public int dp ;
+
+    /// <summary>
+    /// 武器的名稱
+    /// </summary>
+	[Header("裝備")]
+    public string wp ;
+
+    /// <summary>
+    /// 裝備的名稱
+    /// </summary>
+	 public string wn = ("皮衣");
+
+    /// <summary>
+    /// 是否有帶鑰匙
+    /// </summary>
+    [Header("其他資料"), Tooltip("怪物是否有帶鑰匙")]
+    public bool key = false;
+
+    /// <summary>
+    /// 是否掉落寶物
+    /// </summary>
+    [Tooltip("怪物是否帶有寶物")]
+    public bool tr = false;
+
 }
+
+
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    [Header("怪物一號")]
+    public Enemy enemy1;
+
+    [Header("怪物二號")]
+    public Enemy enemy2;
+
+    private void Start()
+    {
+        print("怪物一號的魔力");
+        print("怪物一號的mp數是" + enemy1.mp);
+        
+        print("怪物二號的裝備名稱");
+        print("怪物二號的裝備名稱" + enemy2.wn);
+
+        enemy1.hp= 100;
+        enemy2.hp = 300;
+        enemy1.dp = 50;
+        enemy1.wn = "木棍";
+        enemy2.wn = "短刀";
+        enemy1.key = true;
+        enemy2.tr = true;
+
+    }
+
+
+}
